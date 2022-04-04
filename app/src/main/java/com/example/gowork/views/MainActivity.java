c22460b091cb6db53d5e0bb91ef522456bca6464
Junha Hwang <junha5021301@kakao.com>
2022-04-04 23:34:37 +0900

    [FEAT] Make Navigation Bar

--- app/src/main/java/com/example/gowork/views/MainActivity.java
+++ app/src/main/java/com/example/gowork/views/MainActivity.java
@@ -1,18 +1,28 @@
 package com.example.gowork.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.example.gowork.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        NavigationUI.setupWithNavController(bottomNav, navController);
 
     }
+
 }
