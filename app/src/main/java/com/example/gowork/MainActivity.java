da224efde86a3b8d6bf6a7a17bb4f62f522a22c8
Junha Hwang <junha5021301@kakao.com>
2022-08-04 16:25:32 +0900

    Make Basic Structure

---
+++ app/src/main/java/com/example/gowork/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.gowork;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NavHostFragment navHostFragment;
+    private NavController navController;
+    private BottomNavigationView bottomNav;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        bottomNav = findViewById(R.id.bottomNavigationView);
+        NavigationUI.setupWithNavController(bottomNav, navController);
+    }
+}
